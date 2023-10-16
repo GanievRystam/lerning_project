8dd041da0183cae76d07b141ce668eb7fef26ef1
Ganiev Rustam <ganiev.rystam@gmail.com>
2023-10-16 21:45:13 +0300

    actions

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -3,8 +3,8 @@ import HtmlWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { type BuildOptions } from './types/config';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
-export function buildPlugins ({ paths }: BuildOptions): webpack.WebpackPluginInstance[] {
-  return [
+export function buildPlugins ({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
+  const plugins = [
     new webpack.ProgressPlugin(),
     new HtmlWebpackPlugin({
       template: paths.html
@@ -12,8 +12,13 @@ export function buildPlugins ({ paths }: BuildOptions): webpack.WebpackPluginIns
     new MiniCssExtractPlugin({
       filename: 'css/[name].[contenthash:8].css',
       chunkFilename: 'css/[name].[contenthash:8].css'
-    }),
-    new webpack.HotModuleReplacementPlugin(),
-    new BundleAnalyzerPlugin()
-  ]
+    })
+  ];
+  if(isDev) {
+    plugins.push(new webpack.HotModuleReplacementPlugin());
+    plugins.push(new BundleAnalyzerPlugin({
+      openAnalyzer: false
+    }));
+  }
+  return plugins
 }
